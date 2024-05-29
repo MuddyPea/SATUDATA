f9a3d1866a67eee06208c1ac494878620e327f2e
MuddyPea <133812983+MuddyPea@users.noreply.github.com>
2024-05-29 15:39:30 +0700

    Add files via upload

---
+++ ADMIN/index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Statistik Sektoral Kota Prabumulih</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logoweb.png" rel="icon">
+  <link href="assets/img/logoweb2.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.php" class="logo d-flex align-items-center">
+        <img src="assets/img/logopbm.jpg" alt="">
+        <h1>Statistik Sektoral</h1>
+      </a>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.php" class="active">Home</a></li>
+          <li><a href="pendidikan.php">Pendidikan</a></li>
+          <li><a href="kesehatan.php">Kesehatan</a></li>
+          <li><a href="pertanian.php">Pertanian</a></li>
+          <li><a href="koperasi.php">Koperasi dan UKM</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="ADMIN/admin.php">DATASET</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+  <!-- End Header -->
+
+  <!-- ======= Call To Action Section ======= -->
+  <section id="call-to-action" class="call-to-action">
+    <div class="container" data-aos="zoom-out">
+
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h3>STATISTIK SEKTORAL KOTA PRABUMULIH</h3>
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Call To Action Section -->
+
+
+  <main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <section id="service" class="services pt-0">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <span>DATA STATISTIK SEKTORAL</span>
+          <h2>DATA STATISTIK SEKTORAL</h2>
+
+        </div>
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/pendidikan2.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="pendidikan.php" class="stretched-link">PENDIDIKAN</a></h3>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/kesehatan2.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="kesehatan.php" class="stretched-link">KESEHATAN</a></h3>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/pertanian2.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="pertanian.php" class="stretched-link">PERTANIAN</a></h3>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="card">
+              <div class="card-img">
+                <img src="assets/img/koperasi2.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service-details.php" class="stretched-link">KOPERASI DAN UKM</a></h3>
+            </div>
+          </div><!-- End Card Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+      <div class="container">
+
+        <div class="slides-1 swiper" data-aos="fade-up">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <p>
+                  <i class="bi bi-quote quote-icon-left"></i>
+                  Berdasarkan Undang-Undang Republik Indonesia Nomor 16 Tahun 1997 tentang statistik, Statistik sektoral
+                  adalah statistik yang pemanfaatannya ditujukan untuk memenuhi kebutuhan instansi tertentu dalam rangka
+                  penyelenggaraan tugas-tugas pemerintahan dan pembangunan yang merupakan tugas pokok instansi yang
+                  bersangkutan.
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <p>
+                  <i class="bi bi-quote quote-icon-left"></i>
+                  Statistik sektoral diselenggarakan oleh instansi pemerintah sesuai lingkup tugas dan fungsinya, secara
+                  mandiri atau bersama dengan Badan.
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <p>
+                  <i class="bi bi-quote quote-icon-left"></i>
+                  Statistik sektoral harus diselenggarakan bersama dengan Badan apabila statistik tersebut hanya dapat
+                  diperoleh dengan cara sensus dan dengan jangkauan populasi berskala nasional.
+                </p>
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <p>
+                  <i class="bi bi-quote quote-icon-left"></i>
+                  Hasil statistik sektoral yang diselenggarakan sendiri oleh instansi pemerintah wajib diserahkan kepada
+                  Badan.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+
+        <div class="row gy-4 align-items-center features-item" data-aos="fade-up">
+
+          <div class="col-md-5">
+            <img src="assets/img/kominfopbm.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-md-7">
+            <p class="fst-italic">
+              Dalam melaksanakan tugasnya, Dinas Komunikasi dan Informatika dibagi menjadi bidang bidang sesuai dengan
+              kepentingan. Salah satu bidang yang dibentuk adalah bidang persandian dan statistik sektoral. Adapun
+              tugasnya yaitu mengenai:
+            </p>
+            <ul>
+              <li><i class="bi bi-check"></i>Penyusunan rencana program kerja dan kegiatan teknis serta penetapan
+                kinerja dibidang statistik sektoral dan pengolahan data</li>
+              <li><i class="bi bi-check"></i>Pelaksanaan pembinaan, pengendalian, pengembangan dan pengelolaan dibidang
+                statistik sektoral dan pengolahan data</li>
+              <li><i class="bi bi-check"></i>Pelaksanaan pengembangan sistem database dan penerapan serta koordinasi
+                pengembangan teknologi statistik sektoral dan pengolahan data Pemerintah Kota</li>
+              <p class="fst-italic">
+                Data-data Statistik Sektoral ini bersumber dari Organisasi Perangkat Daerah (OPD) terkait dan Badan
+                Pusat Statistik Kota Prabumulih
+              </p>
+              <a href="https://kotaprabumulih.go.id" class="hpimg"><img src="assets/img/webkotapbm.png" alt=""
+                  style="width: 30%;"></a>
+              <a href="https://www.kotaprabumulih.go.id/badan-statistik/" class="hpimg"><img
+                  src="assets/img/data statistik.png" alt="" style="width: 30%;"></a>
+            </ul>
+          </div>
+        </div><!-- Features Item -->
+      </div>
+    </section><!-- End Features Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-8 col-md-12 footer-info">
+          <a href="index.php" class="logo d-flex align-items-center">
+            <span>Statistik Sektoral Kota Prabumulih</span>
+          </a>
+          <p>Dikelola Oleh Dinas Komunikasi dan Informatika Kota Prabumulih</p>
+          <div class="social-links d-flex mt-4">
+            <a href="https://www.kotaprabumulih.go.id" class="globe" target="_blank"><i class="bi bi-globe"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100064303353569" class="facebook" target="_blank"><i
+                class="bi bi-facebook"></i></a>
+            <a href="https://www.instagram.com/diskominfoprabumulih" class="instagram" target="_blank"><i
+                class="bi bi-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </footer><!-- End Footer -->
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
